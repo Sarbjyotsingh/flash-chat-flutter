7fa1391848805556358ba2cb9e0456e11672460e
Sarbjyotsingh <chahalsarbjyotsingh@gmail.com>
2020-02-18 17:18:54 +0530

    Prepackaged Animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flutter/material.dart';
 
@@ -48,10 +49,11 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
+                TypewriterAnimatedTextKit(
+                  speed: Duration(seconds: 1),
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 35.0,
                     fontWeight: FontWeight.w900,
                   ),
                 ),
